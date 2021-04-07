15c9808b11d93df16081162ec03cbe7ec89d9757
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-04-07 23:16:11 +0200

    No scroll if menu open

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'areito';
+    title = "areito";
+    menuOpen:boolean = false;
+
+    toggleMenu = (value: boolean) => {
+        this.menuOpen = value;
+
+        if (this.menuOpen) {
+            document.body.classList.add('menu-open');
+        } else {
+            document.body.classList.remove('menu-open');
+        }
+    };
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: 'app-menu',
-    templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.css']
+    selector: "app-menu",
+    templateUrl: "./menu.component.html",
+    styleUrls: ["./menu.component.css"],
 })
-export class MenuComponent {}
+export class MenuComponent {
+    @Output() menuToggled = new EventEmitter<boolean>();
+
+    menuOpen: boolean = false;
+
+    toggleMenu = () => {
+        this.menuOpen = !this.menuOpen;
+        this.menuToggled.emit(this.menuOpen);
+    };
+}
