4094e25e5ea044378d4040d7c1ca7f044801c122
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-04-17 14:06:09 +0200

    feed scrolls down afterviewchecked and after message send

--- src/app/chat/chat-feed/chat-feed.component.ts
+++ src/app/chat/chat-feed/chat-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from "@angular/core";
+import { Component, OnInit, OnChanges, AfterViewChecked } from "@angular/core";
 import { ChatService } from "../chat.service";
 import { Observable } from "rxjs";
 
@@ -7,16 +7,26 @@ import { Observable } from "rxjs";
     templateUrl: "chat-feed.component.html",
     styleUrls: ["chat-feed.component.css"],
 })
-export class ChatFeedComponent implements OnInit, OnChanges {
+export class ChatFeedComponent implements OnInit, OnChanges, AfterViewChecked {
     feed: Observable<any[]>;
 
     constructor(private chatService: ChatService) {}
-
+    
     ngOnInit() {
         this.feed = this.chatService.getMessages();
     }
 
     ngOnChanges() {
         this.feed = this.chatService.getMessages();
+        this.scrollDownFeed();
+    }
+
+    ngAfterViewChecked() {
+        this.scrollDownFeed();
+    }
+
+    scrollDownFeed = () => {
+        const feedEl = <HTMLElement> document.querySelector('.feedWrapper');
+        feedEl.scrollTop = feedEl.scrollHeight;
     }
 }
