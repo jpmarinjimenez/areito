8c52537f98ee0592dafcc394a810cdaa487ba183
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-04-22 17:13:03 +0200

    facebooksigup logic moved to component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,35 @@
-import { Component } from "@angular/core";
-import { AuthService } from "./auth.service";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+import { AuthService } from './auth.service';
 
 @Component({
-    selector: "app-auth",
-    templateUrl: "auth.component.html",
-    styleUrls: ["auth.component.css"],
+    selector: 'app-auth',
+    templateUrl: 'auth.component.html',
+    styleUrls: ['auth.component.css'],
 })
 export class AuthComponent {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private firebaseService: FirebaseService, private router: Router) {}
 
     facebookSignInPopup = () => {
-        this.authService.facebookSignInPopup();
+        this.authService
+            .facebookSignInPopup()
+            .then((result) => {
+                // const credential = result.credential as firebase.auth.OAuthCredential;
+                const user = {
+                    email: result.user.email,
+                    displayName: 'Prueba',
+                    status: 'online',
+                };
+
+                this.firebaseService.getUserData().subscribe((userData) => {
+                    if (!userData) {
+                        this.firebaseService.setUserData(user, result.user.uid);
+                    }
+                });
+
+                this.router.navigate(['/chat']);
+            })
+            .catch((error) => console.log(error));
     };
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import { FirebaseService } from '../services/firebase.service';
 
 @Injectable({
     providedIn: 'root',
@@ -10,40 +8,11 @@ import { FirebaseService } from '../services/firebase.service';
 export class AuthService {
     provider = new firebase.auth.FacebookAuthProvider();
 
-    constructor(private firebaseService: FirebaseService, private router: Router) {}
+    constructor() {}
 
     // Facebook
     facebookSignInPopup() {
-        firebase
-            .auth()
-            .signInWithPopup(this.provider)
-            .then((result) => {
-                // const credential = result.credential as firebase.auth.OAuthCredential;
-                const user = {
-                    email: result.user.email,
-                    displayName: 'Prueba',
-                    status: 'online',
-                };
-
-                this.firebaseService.getUserData().subscribe((userData) => {
-                    if (!userData) {
-                        this.firebaseService.setUserData(user, result.user.uid);
-                    }
-                });
-
-                this.router.navigate(['/chat']);
-            })
-            .catch((error) => {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-
-                // ...
-            });
+        return firebase.auth().signInWithPopup(this.provider);
     }
 
     // Logout
