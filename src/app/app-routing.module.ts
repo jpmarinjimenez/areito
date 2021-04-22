1c15637477bb3452a7d6f439f0512638e133a904
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-04-22 22:04:26 +0200

    iframe chat and social removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
-import { ChatIframeComponent } from "./chatIframe/chatIframe.component";
 import { ChatComponent } from "./chat/chat.component";
-import { SocialComponent } from "./social/social.component";
+
 import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    // { path: 'chat-iframe', component: ChatIframeComponent },
     { path:'chat', component: ChatComponent, canActivate:[AuthGuard] },
-    // { path: 'social', component: SocialComponent }
 ];
 
 @NgModule({
