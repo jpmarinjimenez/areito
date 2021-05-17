bc7fd4296e5af8eea4320c0d9d3f2cd54996981d
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-05-17 17:46:55 +0200

    Pwa fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,12 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
         FormsModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFirestoreModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the app is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
